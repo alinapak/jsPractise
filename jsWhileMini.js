70fec2e6230feade05f23c6e06a5123e24d18247
alinapak <alina.pakamoryte@gmail.com>
2022-04-07 19:07:26 +0300

    while mini

---
+++ jsWhileMini.js
@@ -0,0 +1,55 @@
+//While Mini doesn't fixed
+
+
+//first task .... kiek tikrinu, kartais būna virš 100...
+// let sum=0;
+// let array = [];
+// do {
+// let n = (Math.floor(Math.random() * 10) + 1);
+// sum= sum+n;
+// array.push(n);
+//  }
+// while (sum< 100);
+// console.log(array);
+
+//second task
+// let sum = 0;
+// let array = [];
+// do {
+//    let n = (Math.floor(Math.random() * 10) + 1);
+//    sum = sum + n;
+//    array.push(n);
+// }
+// while ((sum % 7) !== 0);
+// console.log(array);
+
+//third task
+// m = skyriai iš viso 
+// function Tadas(m) {
+//   let d=6; //dienos
+//   let s; //vidutiniškai skyrių per dieną
+//   s = m/d; 
+// do {d--;
+//    m=m-d;
+//    console.log(s);
+// } while (m===0); 
+// }
+// Tadas(17);
+
+//fourth task
+// do {
+// i = (Math.floor(Math.random() * 11));
+// console.log(i);
+// }
+// while (i!==5);
+
+//fifth task
+let array = []
+do {
+   i = (Math.floor(Math.random() * 11));
+   console.log(i);
+   array.push(i)
+   }
+   while (i!==7 && i!==5);
+   console.log("==== how many loops ====");
+   console.log(array.length);
